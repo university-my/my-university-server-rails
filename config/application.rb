c6b01785ed41f5c089bb7c933fa395603f09ea6f
Yura Voevodin <voevodin.yura@icloud.com>
2019-02-17 13:44:41 +0200

    Custom error pages.

--- config/application.rb
+++ config/application.rb
@@ -12,6 +12,8 @@ module MyUniversityServer
     config.load_defaults 5.2
 
     config.i18n.default_locale = :uk
+    
+    config.exceptions_app = self.routes
 
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration can go into files in config/initializers
--- config/routes.rb
+++ config/routes.rb
@@ -9,5 +9,8 @@ Rails.application.routes.draw do
     resources :groups, only: [:index, :show]
   end
   
+  match "/404", :to => "errors#not_found", :via => :all
+  match "/500", :to => "errors#internal_server_error", :via => :all
+  
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
