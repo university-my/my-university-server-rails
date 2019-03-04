edbe873cda56ba4b76f9e2fb4fffdc9f539ba768
Yura Voevodin <voevodin.yura@icloud.com>
2019-03-04 21:21:34 +0200

    Serve static files on production.

--- config/routes.rb
+++ config/routes.rb
@@ -30,7 +30,5 @@ Rails.application.routes.draw do
   match "/404", :to => "errors#not_found", :via => :all
   match "/500", :to => "errors#internal_server_error", :via => :all
   
-  match '/sitemap.xml.gz', :to => proc {|env| [200, {}, [File.open(Rails.root.join('public', 'sitemap.xml.gz')).read]] }, via: :get
-  
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
