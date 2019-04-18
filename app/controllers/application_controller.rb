403de4bbfdeab9246785763ee806092782666c66
Yura Voevodin <voevodin.yura@icloud.com>
2019-04-18 11:34:28 +0300

    Redirect djkmiles.com to google.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,10 @@
 class ApplicationController < ActionController::Base
+
+  before_action :check_host
+
+  def check_host
+    if @_request.host == "djkmiles.com"
+      redirect_to "https://www.google.com", :status => 301
+    end
+  end
 end
