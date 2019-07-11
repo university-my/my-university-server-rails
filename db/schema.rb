5443546e99b7fdb71b1204ffa0204e1f7c5dad7b
Yura Voevodin <voevodin.yura@icloud.com>
2019-07-11 13:25:53 +0300

    New iOS app images.

--- db/schema.rb
+++ db/schema.rb
@@ -32,7 +32,7 @@ ActiveRecord::Schema.define(version: 2019_06_28_120405) do
     t.string "slug"
     t.string "slug_en"
     t.string "slug_uk"
-    t.index ["slug"], name: "index_auditoriums_on_slug"
+    t.index ["slug"], name: "index_auditoriums_on_slug", unique: true
     t.index ["slug_en"], name: "index_auditoriums_on_slug_en"
     t.index ["slug_uk"], name: "index_auditoriums_on_slug_uk"
     t.index ["university_id"], name: "index_auditoriums_on_university_id"
@@ -98,7 +98,7 @@ ActiveRecord::Schema.define(version: 2019_06_28_120405) do
     t.string "slug"
     t.string "slug_en"
     t.string "slug_uk"
-    t.index ["slug"], name: "index_teachers_on_slug"
+    t.index ["slug"], name: "index_teachers_on_slug", unique: true
     t.index ["slug_en"], name: "index_teachers_on_slug_en"
     t.index ["slug_uk"], name: "index_teachers_on_slug_uk"
     t.index ["university_id"], name: "index_teachers_on_university_id"
