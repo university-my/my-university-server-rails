ed64a350327c00bfa401c0bc102df09d7f676a30
Yura Voevodin <voevodin.yura@icloud.com>
2019-08-13 18:58:30 +0300

    Request specialities for departments (KHNEU).

--- app/services/khneu_service.rb
+++ app/services/khneu_service.rb
@@ -10,15 +10,17 @@ module KhneuService
 
   # bin/rails runner 'KhneuService.import_teachers'
   def self.import_teachers
-    # Peform network request and parse JSON
-    # url = "http://schedule.sumdu.edu.ua/index/json?method=getTeachers"
-    # json = ApplicationRecord.perform_request(url)
+    # Departments
+    departments_ids = request_departments()
+
+    # Specialities
+    # request_specialities(departments_ids)
   end
 
 
   # bin/rails runner 'KhneuService.request_departments'
   def self.request_departments
-    # Peform network request and parse JSON
+    # Peform network request and parse XML
     url = "http://services.ksue.edu.ua:8081/schedule/xmlmetadata?q=departments&auth=test"
     
     # Init URI
@@ -44,9 +46,62 @@ module KhneuService
 
     doc = Nokogiri::XML(response.body)
 
+    departments_ids = []
+
     @doc = doc.xpath('//element').each do |department|
       department_id = department.attributes['id'].value
-      department_name = department.at('displayName').text
+
+      p department
+
+      departments_ids.push(department_id)
+    end
+
+    p 'departments_ids = '
+    p departments_ids
+
+    # IDs of all departments
+    return departments_ids
+
+  end
+
+  def self.request_specialities(departments_ids)
+    p '---------'
+    departments_ids.each do |id| 
+      request_specialty(id)
+    end
+  end
+
+  def self.request_specialty(department_id)
+    # Peform network request and parse XML
+    url = "http://services.ksue.edu.ua:8081/schedule/xmlmetadata?q=specialities&facultyid=#{department_id}&auth=test"
+
+    p url
+
+    # Init URI
+    uri = URI(url)
+
+    if uri.nil?
+      # Add error
+      error_message = "Invalid URI"
+      # Log invalid URI
+      Rails.logger.error(error_message)
+      return
+    end
+
+    # Perform request
+    response = Net::HTTP.get_response(uri)
+    if response.code != '200'
+      # Add error
+      error_message = "Server responded with code #{response.code} for GET #{uri}"
+      # Log invalid URI
+      Rails.logger.error(error_message)
+      return
+    end
+
+    doc = Nokogiri::XML(response.body)
+
+    @doc = doc.xpath('//element').each do |specialty|
+      p specialty
     end
 
   end
