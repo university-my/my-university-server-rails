833cf4068b5002e6911636349ead0779fbe351f3
Yura Voevodin <voevodin.yura@icloud.com>
2019-08-16 22:56:05 +0300

    Random record on the home page.

--- app/controllers/homepage_controller.rb
+++ app/controllers/homepage_controller.rb
@@ -1,5 +1,11 @@
 class HomepageController < ApplicationController
   def home
     @title = "Мій Університет"
+    
+    # Find random recod
+    random_record = Record.find(Record.pluck(:id).sample)
+    @university = random_record.university
+    @records = [random_record]
+    @records_days = @records.group_by { |t| t.start_date }
   end
 end
