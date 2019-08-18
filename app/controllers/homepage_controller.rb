025e8f6416631779a9da86aed75098e21d460680
Yura Voevodin <voevodin.yura@icloud.com>
2019-08-18 22:18:03 +0300

    Show random record on the home page if records not empty.

--- app/controllers/homepage_controller.rb
+++ app/controllers/homepage_controller.rb
@@ -3,9 +3,12 @@ class HomepageController < ApplicationController
     @title = "Мій Університет"
     
     # Find random recod
-    random_record = Record.find(Record.pluck(:id).sample)
-    @university = random_record.university
-    @records = [random_record]
-    @records_days = @records.group_by { |t| t.start_date }
+    firstRecord = Record.first
+    if firstRecord
+      random_record = Record.find(Record.pluck(:id).sample)
+      @university = random_record.university
+      @records = [random_record]
+      @records_days = @records.group_by { |t| t.start_date }
+    end
   end
 end
