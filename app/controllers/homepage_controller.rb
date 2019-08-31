35b5a7a89f1d11a9c367e37d3afc24c58a2550b1
Yura Voevodin <voevodin.yura@icloud.com>
2019-08-31 17:17:28 +0300

    Updated query for display random record on the home screen.

--- app/controllers/homepage_controller.rb
+++ app/controllers/homepage_controller.rb
@@ -3,9 +3,10 @@ class HomepageController < ApplicationController
     @title = "Мій Університет"
     
     # Find random recod
-    firstRecord = Record.first
-    if firstRecord
-      random_record = Record.find(Record.pluck(:id).sample)
+    random_record = Record.where(university_id: 1)
+      .where("start_date > ?", DateTime.current)
+      .order("RANDOM()").first
+    if random_record
       @university = random_record.university
       @records = [random_record]
       @records_days = @records.group_by { |t| t.start_date }
