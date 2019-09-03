4cb1a50dadc96d5d40519885b880ddfbb4e22fb6
Yura Voevodin <voevodin.yura@icloud.com>
2019-09-03 18:49:25 +0300

    Working on import groups from KHNEU.

--- app/services/khneu_service.rb
+++ app/services/khneu_service.rb
@@ -10,18 +10,18 @@ module KhneuService
 
   # bin/rails runner 'KhneuService.import_teachers'
   def self.import_teachers
-    # Departments
-    departments_ids = request_departments()
+    # Faculties
+    faculties_ids = request_faculties()
 
     # Specialities
-    # request_specialities(departments_ids)
+    request_specialities(faculties_ids)
   end
 
 
-  # bin/rails runner 'KhneuService.request_departments'
-  def self.request_departments
+  # bin/rails runner 'KhneuService.request_faculties'
+  def self.request_faculties
     # Peform network request and parse XML
-    url = "http://services.ksue.edu.ua:8081/schedule/xmlmetadata?q=departments&auth=test"
+    url = "http://services.ksue.edu.ua:8081/schedule/xmlmetadata?q=faculties&auth=test"
     
     # Init URI
     uri = URI(url)
@@ -46,36 +46,34 @@ module KhneuService
 
     doc = Nokogiri::XML(response.body)
 
-    departments_ids = []
+    faculties_ids = []
 
-    @doc = doc.xpath('//element').each do |department|
-      department_id = department.attributes['id'].value
-
-      p department
-
-      departments_ids.push(department_id)
+    @doc = doc.xpath('//element').each do |faculty|
+      faculty_id = faculty.attributes['id'].value
+      faculties_ids.push(faculty_id)
     end
 
-    p 'departments_ids = '
-    p departments_ids
-
-    # IDs of all departments
-    return departments_ids
+    # IDs of all faculties
+    return faculties_ids
 
   end
 
-  def self.request_specialities(departments_ids)
-    p '---------'
-    departments_ids.each do |id| 
-      request_specialty(id)
+  def self.request_specialities(faculties_ids)
+
+    faculties_ids.each do |faculty_id| 
+      specialities_ids = request_specialty(faculty_id)
+
+      specialities_ids.each do |specialty_id|
+      for course in 1..6 do
+        request_groups(faculty_id, specialty_id, course)
+      end
+    end
     end
   end
 
-  def self.request_specialty(department_id)
+  def self.request_specialty(faculty_id)
     # Peform network request and parse XML
-    url = "http://services.ksue.edu.ua:8081/schedule/xmlmetadata?q=specialities&facultyid=#{department_id}&auth=test"
-
-    p url
+    url = "http://services.ksue.edu.ua:8081/schedule/xmlmetadata?q=specialities&facultyid=#{faculty_id}&auth=test"
 
     # Init URI
     uri = URI(url)
@@ -100,10 +98,52 @@ module KhneuService
 
     doc = Nokogiri::XML(response.body)
 
+    specialities_ids = []
+
     @doc = doc.xpath('//element').each do |specialty|
-      p specialty
+      specialty_id = specialty.attributes['id'].value
+      specialities_ids.push(specialty_id)
     end
 
+    # IDs of specialities
+    return specialities_ids
+  end
+
+  def self.request_groups(faculty_id, specialty_id, course)
+      # Peform network request and parse XML
+    url = "http://services.ksue.edu.ua:8081/schedule/xmlmetadata?q=groups&facultyid=#{faculty_id}&specialityid=#{specialty_id}&course=#{course}&auth=test"
+
+    # Init URI
+    uri = URI(url)
+
+    if uri.nil?
+      # Add error
+      error_message = "Invalid URI"
+      # Log invalid URI
+      Rails.logger.error(error_message)
+      return
+    end
+
+    # Perform request
+    response = Net::HTTP.get_response(uri)
+    if response.code != '200'
+      # Add error
+      error_message = "Server responded with code #{response.code} for GET #{uri}"
+      # Log invalid URI
+      Rails.logger.error(error_message)
+      return
+    end
+
+    doc = Nokogiri::XML(response.body)
+
+    @doc = doc.xpath('//element').each do |group|
+      groupID = group.attributes['id'].value
+      # groupName = group.attributes['displayName'].value
+      p '======'
+      p 'groupID = ', groupID
+      p 'group.children = ', group.children
+      # p group
+    end
   end
 
 end
