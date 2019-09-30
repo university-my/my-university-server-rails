8dd0218f06057a5d8f313447e3bd22851ab469dc
Yura Voevodin <voevodin.yura@icloud.com>
2019-09-30 22:22:05 +0300

    New design of home page.

--- app/controllers/homepage_controller.rb
+++ app/controllers/homepage_controller.rb
@@ -1,15 +1,6 @@
 class HomepageController < ApplicationController
   def home
     @title = "Мій Університет"
-    
-    # Find random recod
-    random_record = Record.where(university_id: 1)
-      .where("start_date > ?", DateTime.current)
-      .order("RANDOM()").first
-    if random_record
-      @university = random_record.university
-      @records = [random_record]
-      @records_days = @records.group_by { |t| t.start_date }
-    end
+    @universities = University.where(is_hidden: false).limit(3)
   end
 end
