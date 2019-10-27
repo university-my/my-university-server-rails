325242cb6a39996bb5947398e660c2de1412c3eb
Yura Voevodin <voevodin.yura@icloud.com>
2019-10-27 23:10:30 +0200

    SEO optimizations.

--- config/sitemap.rb
+++ config/sitemap.rb
@@ -2,28 +2,6 @@
 SitemapGenerator::Sitemap.default_host = "https://my-university.com.ua"
 
 SitemapGenerator::Sitemap.create do
-  # Put links creation logic here.
-  #
-  # The root path '/' and sitemap index file are added automatically for you.
-  # Links are added to the Sitemap in the order they are specified.
-  #
-  # Usage: add(path, options={})
-  #        (default options are used if you don't specify)
-  #
-  # Defaults: :priority => 0.5, :changefreq => 'weekly',
-  #           :lastmod => Time.now, :host => default_host
-  #
-  # Examples:
-  #
-  # Add '/articles'
-  #
-  #   add articles_path, :priority => 0.7, :changefreq => 'daily'
-  #
-  # Add all articles:
-  #
-  #   Article.find_each do |article|
-  #     add article_path(article), :lastmod => article.updated_at
-  #   end
 
   # Static pages
   add about_path, :priority => 0.8, :changefreq => 'monthly'
@@ -35,16 +13,12 @@ SitemapGenerator::Sitemap.create do
   add android_path, :priority => 0.8, :changefreq => 'monthly'
 
   # Universities
-  add universities_path, :priority => 0.7, :changefreq => 'monthly'
-
   University.where(is_hidden: false).find_each do |university|
     add university_path(university.url), :lastmod => university.updated_at
   end
 
   # Auditoriums
   University.where(is_hidden: false).find_each do |university|
-    add university_auditoriums_path(university.url), :priority => 0.5, :changefreq => 'daily'
-
     Auditorium.where(university: university).find_each do |auditorium|
       add university_auditorium_path(university.url, auditorium.friendly_id), :lastmod => auditorium.updated_at
     end
@@ -52,8 +26,6 @@ SitemapGenerator::Sitemap.create do
 
   # Groups
   University.where(is_hidden: false).find_each do |university|
-    add university_groups_path(university.url), :priority => 0.5, :changefreq => 'daily'
-
     Group.where(university: university).find_each do |group|
       add university_group_path(university.url, group.friendly_id), :lastmod => group.updated_at
     end
@@ -61,19 +33,8 @@ SitemapGenerator::Sitemap.create do
 
   # Teachers
   University.where(is_hidden: false).find_each do |university|
-    add university_teachers_path(university.url), :priority => 0.5, :changefreq => 'daily'
-
     Teacher.where(university: university).find_each do |teacher|
       add university_teacher_path(university.url, teacher.friendly_id), :lastmod => teacher.updated_at
-    end
-  end
-
-  # Buildings
-  University.where(is_hidden: false).find_each do |university|
-    add university_buildings_path(university.url), :priority => 0.4, :changefreq => 'monthly'
-
-    Building.where(university: university).find_each do |building|
-      add university_building_path(university.url, building.friendly_id), :lastmod => building.updated_at
     end
   end
 
