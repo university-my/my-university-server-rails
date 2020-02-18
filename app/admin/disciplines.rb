249cf7cf6c645e2ba35a5f694bfb50f4515a5dba
Yura Voevodin <voevodin.yura@icloud.com>
2020-02-18 22:42:05 +0200

    Different filters and collumns for different roles.

--- app/admin/disciplines.rb
+++ app/admin/disciplines.rb
@@ -3,12 +3,56 @@ ActiveAdmin.register Discipline do
   permit_params :name, :university_id
 
   # Filterable attributes on the index screen
-  filter :university
   filter :name
+  filter :university, if: proc { current_admin_user.is_admin? }
+  filter :slug_uk, if: proc { current_admin_user.is_admin? }
 
+  # Index
   index do
     column :name
-    column :university
+
+    # University
+    if current_admin_user.is_admin?
+      column t('university') do |discipline|
+        if discipline.university
+          link_to discipline.university.short_name, admin_university_path(discipline.university_id)
+        end
+      end
+    end
+
+    # Columns for admin
+    if current_admin_user.is_admin?
+      column :slug_uk
+      column :created_at
+      column :updated_at
+    end
+
     actions
   end
+
+  # Form
+  form do |f|
+    f.inputs do
+      f.input :name
+
+      # University
+      if current_admin_user.is_admin?
+        f.input :university, as: :select, include_blank: false
+      elsif current_admin_user.is_editor?
+        f.input :university, as: :select, collection: [current_admin_user.university], include_blank: false
+      end
+    end
+    f.actions
+  end
+
+  # Show Page
+  show do
+    attributes_table do
+      row :name
+      row :university
+      row :created_at
+      row :updated_at
+    end
+  end
+
 end
--- app/admin/records.rb
+++ app/admin/records.rb
@@ -1,37 +1,59 @@
 ActiveAdmin.register Record do
 
   permit_params :start_date, :name, :pair_name, :created_at, :updated_at, :reason, :kind, :time, :auditorium_id, :teacher_id, :pair_start_date
-  
+
   actions :all
-  
+
+  # Filterable attributes on the index screen
+  filter :name
+
+  filter :auditorium, collection: proc { Auditorium.where(university: current_admin_user.university).all }
+  filter :group, collection: proc { Group.where(university: current_admin_user.university).all }
+  filter :teacher, collection: proc { Teacher.where(university: current_admin_user.university).all }
+  filter :university, if: proc { current_admin_user.is_admin? }
+  filter :discipline, collection: proc { Discipline.where(university: current_admin_user.university).all }
+
+  # Index
   index do
     selectable_column
     column :pair_start_date
-    column :name
+    column :discipline
 
-    column t('university') do |record|
-      link_to record.university.short_name, admin_university_path(record.university_id)
+    # University
+    if current_admin_user.is_admin?
+      column t('university') do |record|
+        if record.university
+          link_to record.university.short_name, admin_university_path(record.university_id)
+        end
+      end
     end
-    
+
     column :reason
     column :kind
     column :auditorium
     column :teacher
-    
+
     actions
   end
-  
+
+  # Form
   form do |f|
     f.inputs do
+
+      f.input :discipline, as: :select,
+      collection: Discipline.where(university: current_admin_user.university)
+      .collect { |a| [ a.name, a.id ] }
+
       f.input :start_date
-      f.input :name
-      f.input :pair_name
+
       f.input :reason
       f.input :kind
-      f.input :time
-      f.input :auditorium_id
-      f.input :teacher_id
-      f.input :pair_start_date
+      f.input :auditorium, as: :select,
+      collection: Auditorium.where(university: current_admin_user.university)
+      .collect { |a| [ a.name, a.id ] }
+      f.input :teacher, as: :select,
+      collection: Teacher.where(university: current_admin_user.university)
+      .collect { |a| [ a.name, a.id ] }
     end
     f.actions
   end
