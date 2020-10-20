be0fa6f76b940248d964c210a03b02a9b5f1250e
Yura Voevodin <voevodin.yura@icloud.com>
2020-10-20 14:08:07 +0300

    Refactor "SumduService"

--- app/services/sumdu_service.rb
+++ app/services/sumdu_service.rb
@@ -1,27 +1,28 @@
+# frozen_string_literal: true
+
 require 'net/http'
 require 'json'
 
+# Import data for SumDU university
 class SumduService
 
   #
   # Import records for auditorium from SumDU API
   #
   def self.import_records_for_auditorium(auditorium, date)
-
-    start_date = date.strftime("%d.%m.%Y")
-    end_date = (date + 1.day).strftime("%d.%m.%Y")
+    start_date = date.strftime('%d.%m.%Y')
+    end_date = (date + 1.day).strftime('%d.%m.%Y')
 
     url = 'http://schedule.sumdu.edu.ua/index/json?method=getSchedules'
     query = "&id_aud=#{auditorium.server_id}&date_beg=#{start_date}&date_end=#{end_date}"
 
-    # Peform network request and parse JSON
+    # Perform network request and parse JSON
     json = ApplicationRecord.perform_request(url + query)
 
     university = University.sumdu
 
     # Save records
-    for object in json do
-
+    json.each do |object|
       # Get data from JSON
       date_string = object['DATE_REG']
       time = object['TIME_PAIR']
@@ -41,8 +42,8 @@ class SumduService
         group_names = name_group.split(',')
 
         # Groups
-        striped_names = Array.new
-        for group_name in group_names do
+        striped_names = []
+        group_names.each do |group_name|
           striped_names.push(group_name.strip)
         end
         groups = Group.where(university: university, name: striped_names)
@@ -54,11 +55,10 @@ class SumduService
 
         # Get pair date and time
         pair_time = time.split('-').first
-        pair_start_date  = (date_string + ' ' + pair_time).to_datetime
+        pair_start_date = (date_string + ' ' + pair_time).to_datetime
 
         # Save or update Record
         save_or_update_record(pair_start_date, time, name_string, pair_name, reason, kind, auditorium, teacher, groups, university)
-
       rescue StandardError => e
         Rails.logger.error(e)
         next
@@ -67,30 +67,26 @@ class SumduService
 
     # Update `updated_at` date of Auditorium
     auditorium.touch(:updated_at)
-    unless auditorium.save
-      Rails.logger.error(errors.full_messages)
-    end
+    Rails.logger.error(errors.full_messages) unless auditorium.save
   end
 
   #
   # Import records for group from SumDU API
   #
   def self.import_records_for_group(group, date)
-
-    start_date = date.strftime("%d.%m.%Y")
-    end_date = (date + 1.day).strftime("%d.%m.%Y")
+    start_date = date.strftime('%d.%m.%Y')
+    end_date = (date + 1.day).strftime('%d.%m.%Y')
 
     url = 'http://schedule.sumdu.edu.ua/index/json?method=getSchedules'
     query = "&id_grp=#{group.server_id}&date_beg=#{start_date}&date_end=#{end_date}"
 
-    # Peform network request and parse JSON
+    # Perform network request and parse JSON
     json = ApplicationRecord.perform_request(url + query)
 
     university = University.sumdu
 
     # Save records
-    for object in json do
-
+    json.each do |object|
       # Get data from JSON
       date_string = object['DATE_REG']
       time = object['TIME_PAIR']
@@ -118,12 +114,11 @@ class SumduService
 
         # Get pair date and time
         pair_time = time.split('-').first
-        pair_start_date  = (date_string + ' ' + pair_time).to_datetime
+        pair_start_date = (date_string + ' ' + pair_time).to_datetime
 
         # Save or update Record
         save_or_update_record(pair_start_date, time, name_string, pair_name, reason, kind, auditorium, teacher, [group], university)
-
-      rescue Exception => e
+      rescue StandardError => e
         Rails.logger.error(e)
         next
       end
@@ -131,32 +126,28 @@ class SumduService
 
     # Update `updated_at` date of Group
     group.touch(:updated_at)
-    unless group.save
-      Rails.logger.error(errors.full_messages)
-    end
+    Rails.logger.error(errors.full_messages) unless group.save
   end
 
   #
   # Import records for teacher from SumDU API
   #
   def self.import_records_for_teacher(teacher, date)
-
-    start_date = date.strftime("%d.%m.%Y")
-    end_date = (date + 1.day).strftime("%d.%m.%Y")
+    start_date = date.strftime('%d.%m.%Y')
+    end_date = (date + 1.day).strftime('%d.%m.%Y')
 
     # TODO: Check dates
 
     url = 'http://schedule.sumdu.edu.ua/index/json?method=getSchedules'
     query = "&id_fio=#{teacher.server_id}&date_beg=#{start_date}&date_end=#{end_date}"
 
-    # Peform network request and parse JSON
+    # Perform network request and parse JSON
     json = ApplicationRecord.perform_request(url + query)
 
     university = University.sumdu
 
     # Save records
-    for object in json do
-
+    json.each do |object|
       # Get data from JSON
       date_string = object['DATE_REG']
       time = object['TIME_PAIR']
@@ -176,8 +167,8 @@ class SumduService
         group_names = name_group.split(',')
 
         # Groups
-        striped_names = Array.new
-        for group_name in group_names do
+        striped_names = []
+        group_names.each do |group_name|
           striped_names.push(group_name.strip)
         end
         groups = Group.where(university: university, name: striped_names)
@@ -189,12 +180,11 @@ class SumduService
 
         # Get pair date and time
         pair_time = time.split('-').first
-        pair_start_date  = (date_string + ' ' + pair_time).to_datetime
+        pair_start_date = (date_string + ' ' + pair_time).to_datetime
 
         # Save or update Record
         save_or_update_record(pair_start_date, time, name_string, pair_name, reason, kind, auditorium, teacher, groups, university)
-
-      rescue Exception => e
+      rescue StandardError => e
         Rails.logger.error(e)
         next
       end
@@ -202,9 +192,7 @@ class SumduService
 
     # Update `updated_at` date of Teacher
     teacher.touch(:updated_at)
-    unless teacher.save
-      Rails.logger.error(errors.full_messages)
-    end
+    Rails.logger.error(errors.full_messages) unless teacher.save
   end
 
   #
@@ -212,63 +200,54 @@ class SumduService
   #
   # # bin/rails runner 'SumduService.import_auditoriums'
   def self.import_auditoriums
-
-    # Peform network request and parse JSON
-    url = "http://schedule.sumdu.edu.ua/index/json?method=getAuditoriums"
+    # Perform network request and parse JSON
+    url = 'http://schedule.sumdu.edu.ua/index/json?method=getAuditoriums'
     json = ApplicationRecord.perform_request(url)
 
     # This groups for SumDU
     university = University.sumdu
 
-    for object in json do
+    json.each do |object|
+      # Convert to int before save
+      server_id = Integer(object[0])
+      auditorium_name = object[1]
 
-      begin
-        # Convert to int before save
-        server_id = Integer(object[0])
-        auditorium_name = object[1]
+      # Building name
+      building_name = ''
+      if auditorium_name.include? '-'
+        building_name = auditorium_name.split('-').first
+      elsif auditorium_name.include? '_'
+        building_name = auditorium_name.split('_').first
+      end
+      building = Building.where(university: university).where('name LIKE ?', "#{building_name}%").first
 
-        # Building name
-        if auditorium_name.include? "-"
-          building_name = auditorium_name.split('-').first
-        elsif auditorium_name.include? "_"
-          building_name = auditorium_name.split('_').first
-        end
-        if building_name
-          building = Building.where(university: university)
-          .where('name LIKE ?', "#{building_name}%").first
-        end
+      # Conditions for find existing auditorium
+      conditions = {}
+      conditions[:university_id] = university.id
+      conditions[:server_id] = server_id
+      conditions[:name] = auditorium_name
 
-        # Conditions for find existing auditorium
-        conditions = {}
-        conditions[:university_id] = university.id
-        conditions[:server_id] = server_id
-        conditions[:name] = auditorium_name
+      # Try to find existing auditorium first
+      auditorium = Auditorium.find_by(conditions)
 
-        # Try to find existing auditorium first
-        auditorium = Auditorium.find_by(conditions)
+      auditorium = Auditorium.new if auditorium.nil?
+      auditorium.server_id = server_id
+      auditorium.name = auditorium_name
+      auditorium.university = university
 
-        if auditorium.nil?
-          auditorium = Auditorium.new
-        end
-        auditorium.server_id = server_id
-        auditorium.name = auditorium_name
-        auditorium.university = university
+      if auditorium.building.nil?
+        # Assign building if empty
+        auditorium.building = building
+      end
 
-        if auditorium.building.nil?
-          # Assign building if empty
-          auditorium.building = building
-        end
-
-        unless auditorium.save
-          # Go to the next iteration if can't be saved
-          Rails.logger.error auditorium.errors.full_messages
-          next
-        end
-
-      rescue Exception => e
-        Rails.logger.error e
+      unless auditorium.save
+        # Go to the next iteration if can't be saved
+        Rails.logger.error auditorium.errors.full_messages
         next
       end
+    rescue StandardError => e
+      Rails.logger.error e
+      next
     end
   end
 
@@ -277,48 +256,43 @@ class SumduService
   #
   # bin/rails runner 'SumduService.import_groups'
   def self.import_groups
-
-    # Peform network request and parse JSON
-    url = "http://schedule.sumdu.edu.ua/index/json?method=getGroups"
+    # Perform network request and parse JSON
+    url = 'http://schedule.sumdu.edu.ua/index/json?method=getGroups'
     json = ApplicationRecord.perform_request(url)
 
     # This groups for SumDU
     university = University.sumdu
 
-    for object in json do
+    json.each do |object|
+      # Convert to int before save
+      server_id = Integer(object[0])
+      group_name = object[1]
 
-      begin
-        # Convert to int before save
-        server_id = Integer(object[0])
-        group_name = object[1]
+      # Conditions for find existing group
+      conditions = {}
+      conditions[:university_id] = university.id
+      conditions[:server_id] = server_id
+      conditions[:name] = group_name
 
-        # Conditions for find existing group
-        conditions = {}
-        conditions[:university_id] = university.id
-        conditions[:server_id] = server_id
-        conditions[:name] = group_name
+      # Try to find existing group first
+      group = Group.find_by(conditions)
 
-        # Try to find existing group first
-        group = Group.find_by(conditions)
+      if group.nil?
+        # Save new group
+        group = Group.new
+        group.server_id = server_id
+        group.name = group_name
+        group.university = university
 
-        if group.nil?
-          # Save new group
-          group = Group.new
-          group.server_id = server_id
-          group.name = group_name
-          group.university = university
-
-          unless group.save
-            # Go to the next iteration if can't be saved
-            Rails.logger.error(group.errors.full_messages)
-            next
-          end
+        unless group.save
+          # Go to the next iteration if can't be saved
+          Rails.logger.error(group.errors.full_messages)
+          next
         end
-
-      rescue Exception => e
-        Rails.logger.error(e)
-        next
       end
+    rescue StandardError => e
+      Rails.logger.error(e)
+      next
     end
   end
 
@@ -327,49 +301,43 @@ class SumduService
   #
   # bin/rails runner 'SumduService.import_teachers'
   def self.import_teachers
-
-    # Peform network request and parse JSON
-    url = "http://schedule.sumdu.edu.ua/index/json?method=getTeachers"
+    # Perform network request and parse JSON
+    url = 'http://schedule.sumdu.edu.ua/index/json?method=getTeachers'
     json = ApplicationRecord.perform_request(url)
 
     # This teachers for SumDU
     university = University.sumdu
 
-    for object in json do
+    json.each do |object|
+      # Convert to int before save
+      server_id = Integer(object[0])
+      teacher_name = object[1]
 
-      begin
-        # Convert to int before save
-        server_id = Integer(object[0])
-        teacher_name = object[1]
+      # Conditions for find existing teacher
+      conditions = {}
+      conditions[:university_id] = university.id
+      conditions[:server_id] = server_id
+      conditions[:name] = teacher_name
 
-        # Conditions for find existing teacher
-        conditions = {}
-        conditions[:university_id] = university.id
-        conditions[:server_id] = server_id
-        conditions[:name] = teacher_name
+      # Try to find existing teacher first
+      teacher = Teacher.find_by(conditions)
 
-        # Try to find existing teahcer first
-        teacher = Teacher.find_by(conditions)
+      if teacher.nil?
+        # Save new teacher
+        teacher = Teacher.new
+        teacher.server_id = server_id
+        teacher.name = teacher_name
+        teacher.university = university
 
-        if teacher.nil?
-          # Save new teacher
-          teacher = Teacher.new
-          teacher.server_id = server_id
-          teacher.name = teacher_name
-          teacher.university = university
-
-          unless teacher.save
-            # Go to the next iteration if can't be saved
-            Rails.logger.error(teacher.errors.full_messages)
-            next
-          end
+        unless teacher.save
+          # Go to the next iteration if can't be saved
+          Rails.logger.error(teacher.errors.full_messages)
+          next
         end
-
-      rescue Exception => e
-        Rails.logger.error(e)
-        next
       end
-
+    rescue StandardError => e
+      Rails.logger.error(e)
+      next
     end
   end
 
@@ -391,67 +359,33 @@ class SumduService
     record = Record.find_by(conditions)
 
     if record.nil?
-      # Save new record
+      # New record
       record = Record.new
-      record.pair_start_date = pair_start_date
-      record.time = time
-      record.pair_name = pair_name
-      record.name = name_string
-      record.reason = reason
-      record.kind = kind
-      record.university = university
-
-      # Associations
-      record.auditorium = auditorium
-      record.teacher = teacher
-
-      # Push only unique groups
-      for group in groups do
-        unless record.groups.include?(group)
-          record.groups << group
-        end
-      end
-
-      # Save or update Discipline
-      discipline = save_discipline(name_string, auditorium, groups, teacher)
-      record.discipline = discipline
-
-      # Try to save record
-      unless record.save
-        # Go to the next iteration if record can't be saved
-        Rails.logger.error(record.errors.full_messages)
-      end
-
-    else
-      # Update record
-      record.pair_start_date = pair_start_date
-      record.time = time
-      record.pair_name = pair_name
-      record.name = name_string
-      record.reason = reason
-      record.kind = kind
-      record.university = university
-
-      # Associations
-      record.auditorium = auditorium
-      record.teacher = teacher
-
-      # Push only unique groups
-      for group in groups do
-        unless record.groups.include?(group)
-          record.groups << group
-        end
-      end
-
-      # Save or update Discipline
-      discipline = save_discipline(name_string, auditorium, groups, teacher)
-      record.discipline = discipline
-
-      unless record.save
-        # Go to the next iteration if record can't be saved
-        Rails.logger.error(record.errors.full_messages)
-      end
     end
+
+    # Update record
+    record.pair_start_date = pair_start_date
+    record.time = time
+    record.pair_name = pair_name
+    record.name = name_string
+    record.reason = reason
+    record.kind = kind
+    record.university = university
+
+    # Associations
+    record.auditorium = auditorium
+    record.teacher = teacher
+
+    # Push only unique groups
+    groups.each do |group|
+      record.groups << group unless record.groups.include?(group)
+    end
+
+    # Save or update Discipline
+    discipline = save_discipline(name_string, auditorium, groups, teacher)
+    record.discipline = discipline
+
+    return if record.save
   end
 
   #
@@ -460,7 +394,6 @@ class SumduService
   def self.save_discipline(name, auditorium, groups, teacher)
     university = University.sumdu
     discipline = Discipline.save_or_update(name, university, auditorium, groups, teacher)
-    return discipline
+    discipline
   end
-
 end
