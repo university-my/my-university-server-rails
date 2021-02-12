ac3c49d94e691a4bee69164b6f90bad660daeb49
Yura Voevodin <voevodin.yura@icloud.com>
2021-02-12 23:28:25 +0200

    New domain for 'ultimate-schedule-api'

--- app/services/politek_service.rb
+++ app/services/politek_service.rb
@@ -6,7 +6,7 @@ require 'nokogiri'
 class PolitekService
 
   def self.api_url
-    "https://ultimate-api.my-university.com.ua"
+    "https://ultimate-schedule-api-26hfd.ondigitalocean.app"
   end
 
   def self.load_objects(object_type, url)
