df78adcfb7eeba5940834cc5259309a6573a89be
Yura Voevodin <voevodin.yura@icloud.com>
2021-03-20 23:42:42 +0200

    New feature: find records for the next days

--- app/helpers/records_helper.rb
+++ app/helpers/records_helper.rb
@@ -21,11 +21,21 @@ module  RecordsHelper
 
   def self.fetch_records(university, group, pair_date)
     Record.joins(:groups)
-          .where(university: university)
-          .where('groups.id': group.id)
-          .where(pair_start_date: pair_date.all_day)
-          .order(:pair_start_date)
-          .order(:pair_name)
+    .where(university: university)
+    .where('groups.id': group.id)
+    .where(pair_start_date: pair_date.all_day)
+    .order(:pair_start_date)
+    .order(:pair_name)
+  end
+
+  def self.next_records(university, group, pair_date)
+    Record.joins(:groups)
+    .where(university: university)
+    .where('groups.id': group.id)
+    .where("pair_start_date >= :date", date: pair_date.tomorrow.beginning_of_day)
+    .order(:pair_start_date)
+    .order(:pair_name)
+    .limit(1)
   end
 
 end
