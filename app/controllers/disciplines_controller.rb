82480d6a0f4a320de8dfb25133b44214f7ef6ed0
Yura Voevodin <voevodin.yura@icloud.com>
2021-03-21 23:36:58 +0200

    Fixes and improvements after code review

--- app/controllers/disciplines_controller.rb
+++ app/controllers/disciplines_controller.rb
@@ -1,19 +1,17 @@
 class DisciplinesController < ApplicationController
-
   # GET /universities/:university_url/disciplines
   def index
     @university = University.find_by!(url: params[:university_url])
-    per_page = 5
-    @query = params["query"]
+    @query = params['query']
     if @query.present?
       @disciplines = Discipline.where(university: @university)
       .where("visible_name LIKE ?", "%#{@query.downcase}%")
       .order(:visible_name)
-      .paginate(page: params[:page], per_page: per_page)
+      .paginate(page: params[:page], per_page: 5)
     else
       @disciplines = Discipline.where(university: @university)
       .order(:visible_name)
-      .paginate(page: params[:page], per_page: per_page)
+      .paginate(page: params[:page], per_page: 5)
     end
   end
 
@@ -22,5 +20,4 @@ class DisciplinesController < ApplicationController
     @university = University.find_by!(url: params[:university_url])
     @discipline = @university.disciplines.friendly.find(params[:id])
   end
-
 end
